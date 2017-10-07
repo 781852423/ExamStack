b05360ac418519ae913b62a0d94c3440c22969cb
X200 <X200@PC-20160405ZHRF>
2017-10-07 20:31:15 +0800

    更新common中国关于scoreMarker的配置

--- common/src/main/java/com/examstack/common/Constants.java
+++ common/src/main/java/com/examstack/common/Constants.java
@@ -3,6 +3,6 @@ package com.examstack.common;
 public class Constants {
 	
 	public final static String ANSWERSHEET_DATA_QUEUE = "examplus.answersheetToScoreMaker";
-	public final static String CONFIG_PATH = "/opt/scoremarker";
+	public final static String CONFIG_PATH = "/opt/examstack/scoremarker";
 
 }
