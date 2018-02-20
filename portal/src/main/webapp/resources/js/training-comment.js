b2ffeba69c834f417cde4380ea8e82d5cd16782c
jie's xiaomi desktop <781852423@qq.com>
2018-02-20 23:02:19 +0800

    加入鼠标右键禁用的代码

--- portal/src/main/webapp/resources/js/training-comment.js
+++ portal/src/main/webapp/resources/js/training-comment.js
@@ -1,3 +1,92 @@
+ function clickIE4(){  
+         if (event.button==2){  
+                 return false;  
+         }  
+ }  
+    
+ function clickNS4(e){  
+         if (document.layers||document.getElementById&&!document.all){  
+                 if (e.which==2||e.which==3){  
+                         return false;  
+                 }  
+         }  
+ }  
+    
+ function OnDeny(){  
+         if(event.ctrlKey || event.keyCode==78 && event.ctrlKey || event.altKey || event.altKey && event.keyCode==115){  
+                 return false;  
+         }  
+ }  
+    
+ if (document.layers){  
+         document.captureEvents(Event.MOUSEDOWN);  
+         document.onmousedown=clickNS4;  
+         document.onkeydown=OnDeny();  
+ }else if (document.all&&!document.getElementById){  
+         document.onmousedown=clickIE4;  
+         document.onkeydown=OnDeny();  
+ }  
+    
+document.oncontextmenu=new Function("return false");  
+$(document).bind('contextmenu',function(){
+return false;
+});
+function doProhibit()
+{
+ if(window.Event) 
+     document.captureEvents(Event.MOUSEUP);
+   
+ function nocontextmenu() 
+ {
+     event.cancelBubble = true
+     event.returnvalue = false;
+     return false;
+ }
+  
+ function norightclick(e) 
+ {
+     if (window.Event)
+     {
+        if (e.which == 2 || e.which == 3)
+        return false;
+     }
+     else if (event.button == 2 || event.button == 3) 
+     {
+   event.cancelBubble = true
+   event.returnvalue = false;
+   return false;
+     }
+ }
+ document.oncontextmenu = nocontextmenu;  // for IE5+ 
+ document.onmousedown = norightclick;  // 
+}
+
+if (document.layers) 
+{
+ document.captureEvents(Event.MOUSEDOWN);
+}
+document.onmousedown = click;
+document.oncontextmenu = new Function("return false;")
+function click(e) 
+{
+   e = e || event;
+   if (e.button == 2) 
+ {
+     var tag = e.srcElement || e.target;
+    if (tag.type == "text" || tag.type == "textarea") 
+     {
+         document.oncontextmenu = new Function("return true;")
+      }
+       else
+       {
+       document.oncontextmenu = new Function("return false;")
+       }
+   }
+}
+
+/**********above code used to disable right click*************/
+
+
 $(function() {
 	comment.initial();
 	comment.queryComment();
